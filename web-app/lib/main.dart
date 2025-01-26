49446bda9ea160d3746332f6a39235be8f420c60
Ronald Figueroa Avila <ronaldra09gtech@gmail.com>
2025-01-27 01:29:54 +0800

    Firebase Config Web

--- web-app/lib/main.dart
+++ web-app/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tranquilestate_admin_panel/firebase_options.dart';
 import 'app.dart';
 
 /// Entry point of Flutter App
@@ -11,6 +14,8 @@ Future<void> main() async {
   // Remove # sign from url
 
   // Initialize Firebase & Authentication Repository
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+      // .then((_) => Get.put(AuthenticationRepository()));
 
   // Main App Starts here...
   runApp(const App());
