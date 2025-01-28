567505a5540dca08cf69fd33143f61b1e3c8220f
Ronald Figueroa Avila <ronaldra09gtech@gmail.com>
2025-01-28 23:40:30 +0800

    update

--- mobile-app/lib/main.dart
+++ mobile-app/lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tranquilestate/app.dart';
+import 'package:tranquilestate/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  /// Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  /// Load all the Material Design / Themes / Localizations / Bindings
   runApp(const App());
 }
