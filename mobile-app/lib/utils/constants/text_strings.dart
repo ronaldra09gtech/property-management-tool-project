46fb42ac0d868d6a8d40af146333a156bd801da6
Ronald Figueroa Avila <ronaldra09gtech@gmail.com>
2025-02-03 18:14:09 +0800

    Update text_strings.dart

--- mobile-app/lib/utils/constants/text_strings.dart
+++ mobile-app/lib/utils/constants/text_strings.dart
@@ -1,71 +1,64 @@
 /// This class contains all the App Text in String formats.
 class TTexts {
   // -- GLOBAL Texts
-  static const String and = "and";
-  static const String skip = "Skip";
-  static const String done = "Done";
-  static const String submit = "Submit";
-  static const String appName = "TranquilEstate";
-  static const String tContinue = "Continue";
+  static const String and = "そして";
+  static const String skip = "スキップ";
+  static const String done = "終わり";
+  static const String submit = "提出する";
+  static const String appName = "静かな地所";
+  static const String tContinue = "続く";
 
   // -- OnBoarding Texts
-  static const String onBoardingTitle1 = "Choose your Property";
-  static const String onBoardingTitle2 = "Your Ideal Property, Just a Step Away!";
-  static const String onBoardingTitle3 = "Unlock Your Dream Home – Where Comfort Meets Convenience!";
+  static const String onBoardingTitle1 = "物件を選択してください";
+  static const String onBoardingTitle2 = "理想の物件がすぐそこに！";
+  static const String onBoardingTitle3 = "夢のマイホームのロックを解除 – 快適さと利便性が融合した場所!";
 
   static const String onBoardingSubTitle1 =
-      "Discover Endless Possibilities in Real Estate – Find Your Dream Property Today!";
+      "不動産の無限の可能性を発見してください – 今すぐ夢の不動産を見つけてください!";
   static const String onBoardingSubTitle2 =
-      "Find the Perfect Space to Call Home – Designed for You.";
+      "あなたのためにデザインされた、我が家と呼べる完璧な空間を見つけてください。";
   static const String onBoardingSubTitle3 =
-      "Explore a World of Premium Properties Tailored to Your Lifestyle.";
+      "あなたのライフスタイルに合わせたプレミアムな物件の世界を探索してください。";
 
   // -- Authentication Forms
-  static const String firstName = "First Name";
-  static const String lastName = "Last Name";
-  static const String email = "E-Mail";
-  static const String password = "Password";
-  static const String newPassword = "New Password";
-  static const String username = "Username";
-  static const String phoneNo = "Phone Number";
-  static const String rememberMe = "Remember Me";
-  static const String forgetPassword = "Forget Password?";
-  static const String signIn = "Sign In";
-  static const String createAccount = "Create Account";
-  static const String orSignInWith = "or sign in with";
-  static const String orSignUpWith = "or sign up with";
-  static const String iAgreeTo = "I agree to";
-  static const String privacyPolicy = "Privacy Policy";
-  static const String termsOfUse = "Terms of use";
-  static const String verificationCode = "verificationCode";
-  static const String resendEmail = "Resend Email";
-  static const String resendEmailIn = "Resend email in";
+  static const String firstName = "名";
+  static const String lastName = "姓";
+  static const String email = "Eメール";
+  static const String password = "パスワード";
+  static const String newPassword = "新しいパスワード";
+  static const String username = "ユーザー名";
+  static const String phoneNo = "電話番号";
+  static const String rememberMe = "ログイン情報を記憶する";
+  static const String forgetPassword = "パスワードをお忘れですか？";
+  static const String signIn = "サインイン";
+  static const String createAccount = "アカウント作成";
+  static const String orSignInWith = "または次でサインイン";
+  static const String orSignUpWith = "または次で登録";
+  static const String iAgreeTo = "私は同意します";
+  static const String privacyPolicy = "プライバシーポリシー";
+  static const String termsOfUse = "利用規約";
+  static const String verificationCode = "認証コード";
+  static const String resendEmail = "メールを再送信";
+  static const String resendEmailIn = "メールを再送信まで";
 
-  // -- Authentication Headings
-  static const String loginTitle = "Welcome back,";
-  static const String loginSubTitle =
-      "Discover Limitless Choices and Unmatched Convenience.";
-  static const String signupTitle = "Let’s create your account";
-  static const String forgetPasswordTitle = "Forget password";
-  static const String forgetPasswordSubTitle =
-      "Don’t worry sometimes people can forget too, enter your email and we will send you a password reset link.";
-  static const String changeYourPasswordTitle = "Password Reset Email Sent";
-  static const String changeYourPasswordSubTitle =
-      "Your Account Security is Our Priority! We've Sent You a Secure Link to Safely Change Your Password and Keep Your Account Protected.";
-  static const String confirmEmail = "Verify your email address!";
-  static const String confirmEmailSubTitle =
-      "Congratulations! Your Account Awaits: Verify Your Email to Start Shopping and Experience a World of Unrivaled Deals and Personalized Offers.";
-  static const String emailNotReceivedMessage =
-      "Didn’t get the email? Check your junk/spam or resend it.";
-  static const String yourAccountCreatedTitle =
-      "Your account successfully created!";
-  static const String yourAccountCreatedSubTitle =
-      "Welcome to Your Ultimate Shopping Destination: Your Account is Created, Unleash the Joy of Seamless Online Shopping!";
+// -- Authentication Headings
+  static const String loginTitle = "お帰りなさい";
+  static const String loginSubTitle = "無限の選択肢と比類のない利便性を発見してください。";
+  static const String signupTitle = "アカウントを作成しましょう";
+  static const String forgetPasswordTitle = "パスワードを忘れました";
+  static const String forgetPasswordSubTitle = "ご安心ください。誰でも忘れることはあります。メールアドレスを入力すると、パスワードリセットのリンクを送信します。";
+  static const String changeYourPasswordTitle = "パスワードリセットメールを送信しました";
+  static const String changeYourPasswordSubTitle = "アカウントのセキュリティが最優先です！安全なリンクを送信しましたので、パスワードを安全に変更し、アカウントを保護してください。";
+  static const String confirmEmail = "メールアドレスを確認してください！";
+  static const String confirmEmailSubTitle = "おめでとうございます！アカウントが準備完了しました。メールを確認して、ショッピングを始めましょう。";
+  static const String emailNotReceivedMessage = "メールが届きませんでしたか？迷惑メールフォルダを確認するか、再送信してください。";
+  static const String yourAccountCreatedTitle = "アカウントが正常に作成されました！";
+  static const String yourAccountCreatedSubTitle = "究極のショッピング体験へようこそ！アカウントが作成されましたので、シームレスなオンラインショッピングをお楽しみください！";
 
-  // -- Product
-  static const String popularProducts = "Popular Products";
+// -- Product
+  static const String popularProducts = "人気の商品";
 
-  // -- Home
-  static const String homeAppbarTitle = "Good day for Checking Property";
+// -- Home
+  static const String homeAppbarTitle = "物件チェックに良い日です";
   static const String homeAppbarSubTitle = "Ronald Avila";
 }
